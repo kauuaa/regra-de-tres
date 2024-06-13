f46a6b0813095bb52a7461e7d0d932bcde82165f
Kauã Biasebetti <kauamengel17@gmail.com>
2024-06-13 14:19:37 -0300

    estilizacao desenvolvida e bugs arrumados

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
 let resultado = document.getElementById('resultado')
-let mensagemErro = document.getElementById('mensagemErro')
+let erroA = document.getElementById('erroA')
+let erroB = document.getElementById('erroB')
+let erroC = document.getElementById('erroC')
 
 function calcular() {
-    let numberA = Number(document.getElementById('numberA').value)
-    let numberB = Number(document.getElementById('numberB').value)
-    let numberC = Number(document.getElementById('numberC').value)
+    let numberA = document.getElementById('numberA').value
+    let numberB = document.getElementById('numberB').value
+    let numberC = document.getElementById('numberC').value
 
     // Limpa ou atualiza as mensagens de erro conforme o uso do usuário
-    mensagemErro.innerHTML = ''
+    erroA.innerHTML = ''
+    erroB.innerHTML = ''
+    erroC.innerHTML = ''
     
-    // Verifica se o primeiro número é zero
-    if(numberA === 0) {
-        mensagemErro.innerHTML += 'O primeiro valor não pode ser zero!'
-        return
+    // Verifica se os valores digitados não são vazios
+    let erroValor = false
+    if (numberA === '') {
+        erroA.innerHTML = 'Este valor precisa ser preenchido!'
+        erroValor = true
+    }
+
+    if (numberB === '') {
+        erroB.innerHTML = 'Este valor precisa ser preenchido!'
+        erroValor = true
+    }
+
+    if (numberC === '') {
+        erroC.innerHTML = 'Este valor precisa ser preenchido!'
+        erroValor = true
     }
 
     // Verifica se os valores digitados são números 
-    if (isNaN(numberA) || isNaN(numberB) || isNaN(numberC)) {
-        mensagemErro.innerHTML = 'Os valores digitados precisam ser números!'
+    if (!erroValor) {
+        if (isNaN(numberA)) {
+            erroA.innerHTML = 'Este valor precisa ser um número!'
+            erroValor = true
+        }
+
+        if (isNaN(numberB)) {
+            erroB.innerHTML = 'Este valor precisa ser um número!'
+            erroValor = true
+        }
+
+        if (isNaN(numberC)) {
+            erroC.innerHTML = 'Este valor precisa ser um número!'
+            erroValor = true
+        }
+    }
+
+    if (erroValor) {
+        return
+    }
+    
+    // Converte os valores para números após as verificações
+    numberA = Number(numberA)
+    numberB = Number(numberB)
+    numberC = Number(numberC)
+    
+    // Verifica se o primeiro número é zero
+    if (numberA === 0) {
+        erroA.innerHTML = 'O primeiro valor não pode ser zero!'
         return
     }
 
